c2b43846d07b61dbb5343219c264de777372fc2f
sebamarynissen <spamsebakapot@hotmail.com>
2014-05-04 16:57:07 +0200

    Added repository functionality
    Basic Doctrine repository functionality was added

---
+++ lib/ORM/EntityManager.php
@@ -0,0 +1,88 @@
+<?php
+namespace Sea\ORM;
+
+// Dependencies
+use Doctrine\ORM\EntityManager as BaseManager;
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\Configuration;
+use Doctrine\DBAL\Driver\PDOMySql\Driver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Common\EventManager;
+
+/**
+ * An extension of Doctrine's EntityManager
+ *
+ * @author Sebastiaan Marynissen <Sebastiaan.Marynissen@UGent.be>
+ */
+class EntityManager extends BaseManager {
+    
+    /**
+     * Doctrine's configuration object
+     * 
+     * @var Configuration
+     */
+    protected $config;
+    
+    /**
+     * The Doctrine database connection
+     * 
+     * @var Connection
+     */
+    protected $connection;
+    
+    /**
+     * Doctrine's Event Manager
+     * 
+     * @var EventManager
+     */
+    protected $eventManager;
+    
+    /**
+     * Doctrine's PDO driver
+     * 
+     * @var Driver
+     */
+    protected $driver;
+    
+    /**
+     * Doctrine's annotation mapper
+     * 
+     * @var AnnotationDriver
+     */
+    protected $mapper;
+    
+    /**
+     * Creates a specific EntityManager based on a PDO object
+     * 
+     * @param \PDO $pdo A PDO instance to be used
+     * @param string $proxyDir The proxy directory
+     * @param string $proxyNamespace The proxy namespace
+     */
+    public function __construct(\PDO $pdo, $proxyDir, $proxyNamespace) {
+        $this->createConfig($proxyDir, $proxyNamespace);
+        $this->driver = new Driver();
+        $this->connection = new Connection(array(
+            'pdo' => $pdo
+        ), $this->driver);
+        $this->eventManager = new EventManager();
+        parent::__construct($this->connection, $this->config, $this->eventManager);
+    }
+    
+    /**
+     * Creates a doctrine configuration object
+     * 
+     * @param string $dir
+     * @param string $ns
+     * @return EntityManager
+     */
+    protected function createConfig($dir, $ns) {
+        $this->config = new Configuration();
+        $this->mapper = new AnnotationDriver(new AnnotationReader());
+        $this->config->setMetadataDriverImpl($this->mapper);
+        $this->config->setProxyDir($this->getProxyDir());
+        $this->config->setProxyNamespace($this->getProxyNamespace());
+        return $this;
+    }
+    
+}
---
+++ lib/ORM/Repository.php
@@ -0,0 +1,95 @@
+<?php
+namespace Sea\ORM;
+
+use ReflectionClass;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\Common\Annotations\AnnotationReader;
+
+/**
+ * A base class for the different Poutrix repositories
+ *
+ * @author Sebastiaan Marynissen <Sebastiaan.Marynissen@UGent.be>
+ */
+abstract class Repository extends EntityRepository {
+    
+    /**
+     * The entity name the repository is responsible for
+     * 
+     * @var 
+     */
+    protected $entityName;
+    
+    /**
+     * The namespace for the entities
+     * 
+     * @var string
+     */
+    protected $entityNamespace;
+    
+    /**
+     * Constructs a new PoutrixRepository
+     * 
+     * @param \PDO $pdo A PDO instance to use
+     * @param string $entityNamespace The namespace the entities are stored in
+     * @param string $proxyDir Proxy directory
+     * @param string $proxyNamespace Proxy namespace
+     * @throws \Exception
+     */
+    public function __construct(\PDO $pdo, $entityNamespace, $proxyDir, $proxyNamespace) {
+        $this->entityNamespace = $entityNamespace;
+        $this->findEntityName();
+        if (!isset($this->entityName)) {
+            throw new \Exception('No entity name set!');
+        }
+        parent::__construct(new EntityManager($proxyDir, $proxyNamespace), new ClassMetadata($this->entityNamespace . '\\' . $this->entityName));
+    }
+    
+    /**
+     * Saves an entity to the database
+     * 
+     * @param object $entity The entity to save
+     */
+    public function save($entity) {
+        $this->_em->persist($entity);
+        $this->_em->flush();
+        return $this;
+    }
+    
+    /**
+     * Searches for the name the entity is responsible for in the class metadata
+     * 
+     * @return Repository
+     */
+    private function findEntityName() {
+        $class = new ReflectionClass($this);
+        $reader = new AnnotationReader();
+        $name = $reader->getClassAnnotation($class, 'Sea\\ORM\\Annotations\\EntityName');
+        if ($name) {
+            $this->entityName = $name;
+        }
+        return $this;
+    }
+    
+    /**
+     * Should fetch data received from MySQL to an entity
+     *
+    *
+    abstract public function fetch($data);
+    
+    /**
+     * Fetches all entities from a given dataset into an array
+     * 
+     * @param array $data
+     * @return array
+     *
+    public function fetchAll($data) {
+        $entities = array();
+        foreach ($data as $entity) {
+            $fetched = $this->fetch($entity);
+            $entities[$fetched->getId()] = $fetched;
+        }
+        return $entities;
+    }*/
+    
+}
